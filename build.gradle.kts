88340ae36c4d36df5cd7633d20829975f7e4632d
rofor <rofor.jj@gmail.com>
2023-03-11 23:15:48 +0300

    added setting for DependencyUpdatesTask from Gradle Versions Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    repositories { //Specify from which repositories we want to get gradle plugins
+    //Specify from which repositories we want to get gradle plugins
+    repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
         maven { url = uri("https://www.jitpack.io") }
     }
     dependencies {
@@ -16,6 +18,25 @@ allprojects {
         google()
         mavenCentral()
         maven { url = uri("https://www.jitpack.io") }
+    }
+// to use, enter in the terminal:
+// ./gradlew dependencyUpdates
+// or init in AS Tool appName->Tasks->help->dependencyUpdates
+// Report: build/dependencyUpdates/report.txt
+    apply(plugin = "com.github.ben-manes.versions")
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+// reject all non stable versions and disallow release candidates as upgradable versions from stable versions
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
     }
 }
 
